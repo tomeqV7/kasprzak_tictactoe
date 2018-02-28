93a5348dd8b947686e82bfdb674305df3c948a07
tomeqV7 <36509130+tomeqV7@users.noreply.github.com>
2018-02-28 21:09:59 +0100

    Update Board.java

--- app/src/main/java/pl/edu/kasprzak/tictactoe/Board.java
+++ app/src/main/java/pl/edu/kasprzak/tictactoe/Board.java
@@ -13,6 +13,33 @@ public class Board {
         buttons[x][y] = button;
         button.setOnClickListener(new ButtonOnClick(button,this));
     }
+    public void CheckWinner() {
+        counter++;
+            if (buttons[0][0].getText().equals("X") && buttons[1][1].getText().equals("X") && buttons[2][2].getText().equals("X"))
+                WhoWins("X", 0,0,1,1,2,2);
+        
+            if (buttons[0][2].getText().equals("X") && buttons[1][1].getText().equals("X") && buttons[2][0].getText().equals("X"))
+                WhoWins("X",0,2,1,1,2,0);
+        
+            if (buttons[0][0].getText().equals("O") && buttons[1][1].getText().equals("O") && buttons[2][2].getText().equals("O"))
+                WhoWins("O",0,0,1,1,2,2);
+        
+            if (buttons[0][2].getText().equals("O") && buttons[1][1].getText().equals("O") && buttons[2][0].getText().equals("O"))
+                WhoWins("O",0,2,1,1,2,0);
+    }
 }
+for ( int i = 0; i < buttons.length; i++) {
+    if (buttons[0] [i].getText().equals("O") && buttons[1] [i].getText().equals("O") && buttons[2] [i].getText().equals("O")
+       {
+            WhoWins("O",i,0,i,1,i,2);
+          
+       }  else if  (buttons[0] [i].getText().equals("X") && buttons[1] [i].getText().equals("X") && buttons[2] [i].getText().equals("X"))
+           {
+               WhoWins("X",i,0,i,1,i,2);
+           }
+        if (counter ==9) {
+            WhoWins();
+        }
+            }
 
 
